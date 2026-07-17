2151b35734dfb3dc4b3d40489bc9d68c6e7f0c64
neurobomberhikes <neurobomberhikes@gmail.com>
2026-07-17 16:11:27 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'jmt-v9';
+const CACHE = 'jmt-v11';
 const ASSETS = ['./', './index.html', './icon-180.png', './icon-512.png', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
